fde6c7f84213157fb8fc7c1ce1bf0d2ade40f882
Felipe Teixeira <36284538+Felipe4747@users.noreply.github.com>
2022-11-10 21:17:39 -0300

    Update
    Novas Cores e Background
    Timer para não avançar a rodada imediatamente
    Cor verde para sinalizar acerto

--- src/scenes/Game.gd
+++ src/scenes/Game.gd
@@ -8,6 +8,7 @@ var fator2
 var right
 var chances
 var rodada = 0
+var streak = 1
 
 func _ready():
 	Sound_Manager()
@@ -43,12 +44,12 @@ func nextRound():
 func _process(delta):
 	$NinePatchRect/Tempo.text = "Tempo: "+str(round($NinePatchRect/Timer.time_left))
 
-
 func _on_Res_pressed(button: String):
 	#button = Res1, Res2...
-	print(button)
+	if $NextRoundTimer.time_left !=0:
+		return
 	if get_node("NinePatchRect/ResButtons/"+button).text == str(right):
-		rightAnswer()
+		rightAnswer(button)
 	else:
 		wrongAnswer(button)
 		
@@ -56,26 +57,33 @@ func _on_Res_pressed(button: String):
 func setupDifficulty():
 	match Global.difficulty:
 		1:
+			$NinePatchRect/Dificuldade.text = "Fácil"
 			$NinePatchRect/Timer.wait_time = 10
 			minFator = [1,1]
 			maxFator = [5,5]
 		2:
+			$NinePatchRect/Dificuldade.text = "Médio"
 			$NinePatchRect/Timer.wait_time = 15
 			minFator = [1,5]
 			maxFator = [5,10]
 		3:
+			$NinePatchRect/Dificuldade.text = "Difícil"
 			$NinePatchRect/Timer.wait_time = 20
 			minFator = [5,5]
 			maxFator = [10,10]
 
 
-func rightAnswer():
+func rightAnswer(button: String):
+	streak+=1
 	SoundController.playRight()
-	Global.score+=10*chances*round($NinePatchRect/Timer.time_left)
-	nextRound()
+	get_node("NinePatchRect/ResButtons/"+button).modulate = "70ff70"
+	$NinePatchRect/ResButtons/ColorTimer.start()
+	Global.score+=10*streak*round($NinePatchRect/Timer.time_left)
+	$NextRoundTimer.start()
 	pass
 
 func wrongAnswer(button: String):
+	streak=1
 	SoundController.playWrong()
 	get_node("NinePatchRect/ResButtons/"+button).modulate = "ff7070"
 	$NinePatchRect/ResButtons/ColorTimer.start()
@@ -83,7 +91,7 @@ func wrongAnswer(button: String):
 		chances-=1
 		$NinePatchRect/Chances.text = "Chances: "+str(chances)
 	else:
-		nextRound()
+		$NextRoundTimer.start()
 
 func _on_ColorTimer_timeout():
 	for i in range(4):
@@ -91,6 +99,10 @@ func _on_ColorTimer_timeout():
 
 func _on_Timer_timeout():
 	SoundController.playWrong()
+	nextRound()
+	pass # Replace with function body.
+
+func _on_NextRoundTimer_timeout():
 	nextRound()
 	pass # Replace with function body.
 
@@ -105,3 +117,6 @@ func Sound_Manager():
 	else:
 		SoundController.stop_music()
 		$NinePatchRect/Music.icon = ResourceLoader.load("res://src/icons/sound-off.png")
+
+
+
