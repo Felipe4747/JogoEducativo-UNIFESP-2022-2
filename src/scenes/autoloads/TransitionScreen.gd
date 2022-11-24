7603150a0d9a98e33508913316458d91cb76fc8b
Felipe Teixeira <36284538+Felipe4747@users.noreply.github.com>
2022-11-24 15:40:39 -0300

    Correção de Bug e Exe

--- src/scenes/autoloads/TransitionScreen.gd
+++ src/scenes/autoloads/TransitionScreen.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 func change_scene(target: String)->void:
 	$AnimationPlayer.play("fade_to_black")
 	yield($AnimationPlayer, 'animation_finished')
-	
+	SoundController.stop_timer()
 	if get_tree().change_scene(target) != OK:
 		print(get_tree().change_scene(target))
 	
